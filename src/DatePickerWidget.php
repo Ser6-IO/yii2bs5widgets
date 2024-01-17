4e82ad2d6fa329778b99f3b859bdbb49335dff8c
Ser6-IO <100387766+Ser6-IO@users.noreply.github.com>
2024-01-17 11:25:08 -0800

    Update DatePickerWidget.php

--- src/DatePickerWidget.php
+++ src/DatePickerWidget.php
@@ -7,38 +7,39 @@ use yii\base\Widget;
 
 class DatePickerWidget extends Widget
 {
-    public $model;
-    public $caption;
-    public $header = null;
+    public $icon = 'calendar-check';
+    //public $caption = 'YYYY-MM-DD - YYYY-MM-DD';
     public $color = 'info';
-    public $disabled = false;
-    public $items;
+    public $range;
 
-    public $action = 'set-status';
-    public $idField = 'id';
+    public $model;
+    public $range_attribute = 'range';
+    public $from_attribute = 'from';
+    public $to_attribute = 'to';
+    public $formId;
 
     public function run()
     {
-        $datePicker = '<div class="dropdown">
-            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-calendar-check"></i> 2024-01-01 - 2024-01-31
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">Last 24 hours</a></li>
-                <li><a class="dropdown-item" href="#">Yesterday</a></li>
-                <li><a class="dropdown-item" href="#">This week</a></li>
-                <li><a class="dropdown-item" href="#">Last 7 days</a></li>
-                <li><a class="dropdown-item" href="#">This month</a></li>
-                <li><a class="dropdown-item" href="#">Last 30 days</a></li>
-                <li><a class="dropdown-item" href="#">Last 90 days</a></li>
-                <li><a class="dropdown-item" href="#">This year</a></li>
-                <li><a class="dropdown-item" href="#">All time</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Custom range</a></li>
-            </ul>
-        </div>';
+        $rangeInputId = Html::getInputId($this->model, $this->range_attribute);
+        $fromInputId = Html::getInputId($this->model, $this->from_attribute);
+        $toInputId = Html::getInputId($this->model, $this->to_attribute);
 
-        return $datePicker;
+        $rangeHiddenInput = Html::activeHiddenInput($this->model, $this->range_attribute);
+        $fromHiddenInput = Html::activeHiddenInput($this->model, $this->from_attribute);
+        $toHiddenInput = Html::activeHiddenInput($this->model, $this->to_attribute);
+
+
+        $rangeArray = [];
+        foreach ($this->range as $key => $range) {
+            $rangeArray[] = Html::tag('li', Html::a($range, '#', ['class' => 'dropdown-item', 'onclick' => "document.getElementById('$rangeInputId').value = $key; document.getElementById('$this->formId').submit();"]));
+        }
+        $rangeArray[] = Html::tag('li', Html::tag('hr', '', ['class' => 'dropdown-divider']));
+        $rangeArray[] = Html::tag('li', Html::a('Custom range', '#', ['class' => 'dropdown-item']));
+
+        $icon = "<i class='bi bi-$this->icon'></i>";
+        $button = Html::tag('button', "$icon " . $this->range[$this->model->{$this->range_attribute}], ['class' => "btn btn-outline-$this->color dropdown-toggle", 'type' => 'button', 'data-bs-toggle' => 'dropdown']);
+        $ul = Html::tag('ul', implode('', $rangeArray), ['class' => 'dropdown-menu']);
+
+        return Html::tag('div',"$button\n$ul", ['class' => 'dropdown']) . $rangeHiddenInput; // . $fromHiddenInput . $toHiddenInput;
     }
 }
