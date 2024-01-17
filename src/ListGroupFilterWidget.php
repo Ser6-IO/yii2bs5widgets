a85ee01c8793ddd838c57d10cbbcca9e12298b75
Ser6-IO <100387766+Ser6-IO@users.noreply.github.com>
2024-01-17 15:25:38 -0800

    Update ListGroupFilterWidget.php

--- src/ListGroupFilterWidget.php
+++ src/ListGroupFilterWidget.php
@@ -3,28 +3,37 @@
 namespace ser6io\yii2bs5widgets;
 
 use yii\base\Widget;
+use yii\helpers\Url;
 
 class ListGroupFilterWidget extends Widget
 {
     public $title = null;
     public $items = [];
     public $total = 1;
+    public $link = '#';
 
     public function run()
     {
         $listItems = [];
         foreach ($this->items as $row) {
 
+            $percent = round(($row['qty'] / $this->total) * 100);
+
+            if (is_array($this->link) and isset($row['id']))
+                $this->link['id'] = $row['id'];
+                
+            $link = Url::to($this->link);
+                
             $listItems[] =
-            '<a href="#" class="list-group-item list-group-item-action border-bottom-0">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="text-nowrap">' . $row['type'] . '</div>
-                    <span class="badge bg-primary rounded-pill">' . $row['qty'] . '</span>
+            "<a href='$link' class='list-group-item list-group-item-action border-bottom-0'>
+                <div class='d-flex justify-content-between align-items-center'>
+                    <div class='text-nowrap'>$row[type]</div>
+                    <span class='badge bg-primary rounded-pill'>$row[qty]</span>
                 </div>
-                <div class="progress mt-1" role="progressbar" style="height: 2px">
-                    <div class="progress-bar" style="width: ' . round($row['qty']/$this->total*100) . '%"></div>
+                <div class='progress mt-1' role='progressbar' style='height: 2px'>
+                    <div class='progress-bar' style='width: $percent%'></div>
                 </div>
-            </a>';
+            </a>";
         }
         $listItems = implode('', $listItems);
 
