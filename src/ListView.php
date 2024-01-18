7131f1b33589cce0143e0b639a19c9793c639b9b
Ser6-IO <100387766+Ser6-IO@users.noreply.github.com>
2024-01-18 15:39:36 -0800

    Pager formatting

--- src/ListView.php
+++ src/ListView.php
@@ -5,4 +5,14 @@ namespace ser6io\yii2bs5widgets;
 class ListView extends \yii\widgets\ListView
 {
     public $layout = "{items}<div class='d-sm-flex justify-content-between align-items-center'>{summary}{pager}</div>";
+
+    public $pager = [
+        'options' => ['class' => 'pagination my-0'],
+        'linkContainerOptions' => ['class' => 'page-item'],
+        'linkOptions' => ['class' => 'page-link'],
+        'disabledListItemSubTagOptions' => ['class' => 'page-link'],
+        'firstPageLabel' => '&lt;',
+        'lastPageLabel'  => '&gt;',
+        'maxButtonCount' => 5,
+    ];
 }
